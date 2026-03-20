257ce96de2cdd496cabff014728a984e5af4da7c
HichemKhe <khederihichem@gmail.com>
2026-03-20 19:41:34 -0400

    ma partie
    ma partie du projet

--- App/Serveur/app.js
+++ App/Serveur/app.js
@@ -1,15 +1,23 @@
-const app = require("express");
-const port = 3000
-import {db, initializeDatabase} from "./db/db.js"
+const express = require("express");
+const app = express();
+const port = 3000;
+const { initializeDatabase } = require("./db/db.js");
+const authRouter = require("./api/auth.js");
 
 app.use(express.json());
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/../Client/index.html");
 });
 
-
-app.listen(port, () => {
-	console.log("Server is running on port " + port );
-});
+initializeDatabase()
+	.then(() => {
+		app.listen(port, () => {
+			console.log("Server is running on port " + port);
+		});
+	})
+	.catch((err) => {
+		console.error("Error initializing database:", err.message);
+	});
 
