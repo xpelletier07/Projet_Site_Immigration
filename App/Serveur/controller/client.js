1b92b60d10a6637010c0f49c87de3b83307b4aa8
xpelletier07 <xpelletier07@gmail.com>
2026-03-27 15:32:03 -0400

    Update middleware

--- App/Serveur/controller/client.js
+++ App/Serveur/controller/client.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const route = express.Router()
 const {db} = require("../db/db.js")
-const {middleware} = require("../api/authentification/middleware.js")
+const {VerifyToken} = require("../api/authentification/middleware.js")
 const {updateclient} = require("../api/PUT/index.js")
 const multer = require("multer");
 const upload = multer();
 
 // Route pour la modification des données d'un client
-route.put("/update_client/:id" ,middleware, updateclient)
+route.put("/update_client/:id" ,VerifyToken, updateclient)
