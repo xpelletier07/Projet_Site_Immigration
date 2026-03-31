62df1ea219b90011a4ccde9c03e2857f9df3c5f5
Christian Eric <e2479537@cmaisonneuve.qc.ca>
2026-03-31 14:49:25 -0400

    modification des app.js

--- App/Serveur/routes/administrateur.js
+++ App/Serveur/routes/administrateur.js
@@ -1,12 +1,12 @@
-import {db} from("../db/db.js")
-import {verifyRole} from("../api/authentification/middleware.js")
+import {db} from "../db/db.js"
+import {verifyRole} from "../api/authentification/middleware.js" 
 import { inscrireUtilisateur, updateAdmin, deleteAdminById} from "../controller/administrateur.js"
-import express from("express")
+import express from "express" 
 
 const route = express.Router()
 
 // Routes pour la modification des données d'un administrateur
-route.put("/update/:id" ,verifyRole("admin"), updateAdmin())
+route.put("/update/:id" ,verifyRole("admin"), updateAdmin)
 
 // Route pour supprimer un administrateur
 route.delete("/delete/:id", verifyRole("admin"), deleteAdminById)
