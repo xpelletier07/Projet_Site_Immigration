1ecc355e96647d84629c8a8fdcb13ce144d75ab7
xpelletier07 <xpelletier07@gmail.com>
2026-05-03 16:20:12 -0400

    Remove file - APP.jsx

--- App/Client/src/main.jsx
+++ App/Client/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { DashboardClient } from './Client/Dashboard_client.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DashboardClient />
   </StrictMode>,
 )
