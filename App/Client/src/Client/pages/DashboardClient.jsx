1106db6b88e6f2ddbac75d6ddaa808a8f16029b6
Xavier Pelletier <xpelletier07@gmail.com>
2026-05-13 13:14:37 -0400

    Rename SideBar in import

--- App/Client/src/Client/pages/DashboardClient.jsx
+++ App/Client/src/Client/pages/DashboardClient.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import Sidebar from "../components/Sidebar.jsx";
+import Sidebar from "../components/SideBar.jsx";
 import {
 	getClientBundle,
 	uploadDocument,
--- App/Client/src/Client/pages/Documents_Page.jsx
+++ App/Client/src/Client/pages/Documents_Page.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import Sidebar from "../components/Sidebar.jsx";
+import Sidebar from "../components/SideBar.jsx";
 import {
 	getClientBundle,
 	uploadDocument,
