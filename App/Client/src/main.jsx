39a20b77b7bd42af686ea7a767588c47dddab120
Xavier Pelletier <xpelletier07@gmail.com>
2026-05-18 16:27:29 -0400

    Add document in dossier - FIXED

--- App/Client/src/main.jsx
+++ App/Client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import GlobalRouter from './router/Global.router.jsx'
 import { BrowserRouter } from "react-router-dom";
 import Menu from './commun/menu.jsx'
+import { ToastProvider } from './commun/Toast.jsx'
 
 import "./assets/Bulma/css/bulma.min.css";
 import './assets/css/global.css'
@@ -13,8 +14,10 @@ import './assets/css/login.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Menu />
-      <GlobalRouter />
+      <ToastProvider>
+        <Menu />
+        <GlobalRouter />
+      </ToastProvider>
     </BrowserRouter>
   </StrictMode>,
 )
