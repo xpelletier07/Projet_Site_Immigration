212f6a3a405eca63e8e65dd7be728a3484fd7cbb
Christian Eric <e2479537@cmaisonneuve.qc.ca>
2026-05-24 23:03:06 -0400

    construction du component Detailsdossier.jsx

--- App/Client/src/router/DashbordRouter.jsx
+++ App/Client/src/router/DashbordRouter.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import "bulma/css/bulma.min.css";
 //import "@fortawesome/fontawesome-free/css/all.min.css";
 import { AuthContext } from "./AuthContext.jsx";
@@ -7,8 +6,6 @@ import DashBordUtilisateur from "../Utilisateur/pages/DashBordUtilisateur.jsx";
 
 
 export function DashbordRouter() {
-  const [token, settoken] = useState("");
-  const DonneUtils = { token, settoken }
 
   return (
     <>
